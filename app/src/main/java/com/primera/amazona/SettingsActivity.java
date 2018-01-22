17fe02bdf6d7a1d028cc03395b43f8fe894ba384
dreamer8020 <lu.ferx3@gmail.com>
2018-01-22 16:07:42 -0500

    Saves and retrieves data from database 2

---
+++ app/src/main/java/com/primera/amazona/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.primera.amazona;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by queenlu on 11/30/17.
+ */
+
+public class SettingsActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+    }
+
+}
